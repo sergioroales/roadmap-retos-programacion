db103536e52225a88c530a6243faba8b4ab2b53b
SergioRoales <serroales@gmail.com>
2025-02-20 17:21:47 +0100

    resolución Ejercicio 00 por sergio roales

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/sergioroales.py
@@ -0,0 +1,39 @@
+## Ejercicio 1 ## 
+
+"""
+1) Crea un comentario en el código y coloca la URL del sitio web oficial del
+"""
+# https://www.python.org/
+
+"""
+2) Representa las diferentes sintaxis que existen de crear comentarios
+"""
+
+# A) one line
+
+# one line comment
+
+# B) more than one lines
+
+"""
+Three
+Lines
+Comment
+
+"""
+
+"""
+3) Crea una variable (y una constante si el lenguaje lo soporta).
+"""
+
+int_variable = 9
+float_variable = 6.5
+string_variable = "Brais Moure"
+boolean_variable = True
+
+"""
+4) Crea variables representando todos los tipos de datos primitivos
+   del lenguaje (cadenas de texto, enteros, booleanos...).
+"""
+
+print("Hola Python!")
